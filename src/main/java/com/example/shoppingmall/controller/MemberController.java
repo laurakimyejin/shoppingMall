8c557a59f72746dbd3859cc34659c749789db5b5
angpang2 <whtjdcks1111@naver.com>
2022-12-23 16:06:03 +0900

    고객센터 글작성 (기본)

--- src/main/java/com/example/shoppingmall/controller/MemberController.java
+++ src/main/java/com/example/shoppingmall/controller/MemberController.java
@@ -46,4 +46,5 @@ public class MemberController {
         return "/memberPages/myPage";
     }
 
+
 }
--- src/main/java/com/example/shoppingmall/dto/QuestionDTO.java
+++ src/main/java/com/example/shoppingmall/dto/QuestionDTO.java
@@ -1,11 +1,40 @@
 package com.example.shoppingmall.dto;
 
+import com.example.shoppingmall.entity.MemberEntity;
+import com.example.shoppingmall.entity.QuestionEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
 public class QuestionDTO {
+    private Long id;
+    private String questionName;
+    private String questionTitle;
+    private String questionContents;
+    private String questionStatus = "N";
+    private LocalDateTime memberCreatedTime;
+    private LocalDateTime memberUpdatedTime;
+    private Long memberId;
+
+    public static QuestionDTO toDTO(QuestionEntity questionEntity) {
+        QuestionDTO questionDTO = new QuestionDTO();
+        questionDTO.setId(questionEntity.getId());
+        questionDTO.setQuestionName(questionEntity.getQuestionName());
+        questionDTO.setQuestionTitle(questionEntity.getQuestionTitle());
+        questionDTO.setQuestionContents(questionEntity.getQuestionContents());
+        questionDTO.setQuestionStatus(questionEntity.getQuestionStatus());
+        questionDTO.setMemberCreatedTime(questionEntity.getCreatedTime());
+        questionDTO.setMemberUpdatedTime(questionEntity.getUpdatedTime());
+        return questionDTO;
+    }
+
 }
