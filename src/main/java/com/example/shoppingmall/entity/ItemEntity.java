729818dd9a0a94e03ce1f126771dbab3f5c3551a
alswjd094 <alswjd094@gmail.com>
2022-12-23 13:56:35 +0900

    상품등록

--- src/main/java/com/example/shoppingmall/entity/ItemEntity.java
+++ src/main/java/com/example/shoppingmall/entity/ItemEntity.java
@@ -1,14 +1,48 @@
 package com.example.shoppingmall.entity;
 
+import com.example.shoppingmall.dto.ItemDTO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "item_table")
-public class ItemEntity {
+public class ItemEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String itemName;
+
+    @Column(nullable = false)
+    private int itemPrice;
+
+    @Column(length = 500)
+    private String itemContents;
+
+    @Column(length = 100)
+    private String itemImage;
+
+    @Column
+    private int fileAttachedItem;
+
+    @Column(length = 30)
+    private String itemCategory;
+
+    public static ItemEntity toItemSaveEntity(ItemDTO itemDTO){
+        ItemEntity itementity = new ItemEntity();
+        itementity.setItemName(itemDTO.getItemName());
+        itementity.setItemPrice(itemDTO.getItemPrice());
+        itementity.setItemContents(itemDTO.getItemContents());
+        itementity.setItemImage(itemDTO.getItemImage());
+        itementity.setItemCategory(itemDTO.getItemCategory());
+        itementity.setFileAttachedItem(0);
+        return itementity;
+    }
 }
--- src/main/java/com/example/shoppingmall/entity/QuestionEntity.java
+++ src/main/java/com/example/shoppingmall/entity/QuestionEntity.java
@@ -3,12 +3,26 @@ package com.example.shoppingmall.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "question_table")
-public class QuestionEntity {
+public class QuestionEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String questionName;
+
+    @Column(length = 50, nullable = false)
+    private String questionTitle;
+
+    @Column(length = 500)
+    private String questionContents;
+
+    @Column(length = 4, nullable = false)
+    private String questionStatus;
 }
