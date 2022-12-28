2ccbe94bb306aeb73fbfad60c38abbbb42f3ae93
alswjd094 <alswjd094@gmail.com>
2022-12-28 16:45:02 +0900

    회원 정보 수정

--- src/main/java/com/example/shoppingmall/repository/MemberRepository.java
+++ src/main/java/com/example/shoppingmall/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.example.shoppingmall.repository;
 import com.example.shoppingmall.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
     Optional<MemberEntity> findByUserId(String userId);
+
 }
