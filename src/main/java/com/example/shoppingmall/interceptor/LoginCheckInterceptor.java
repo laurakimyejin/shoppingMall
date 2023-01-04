ac377d6a3e2569a60087866eb3ac305bc560a77f
alswjd094 <alswjd094@gmail.com>
2023-01-04 16:38:33 +0900

    인터셉터 수정중

--- src/main/java/com/example/shoppingmall/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/example/shoppingmall/interceptor/LoginCheckInterceptor.java
@@ -1,6 +1,7 @@
 package com.example.shoppingmall.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
