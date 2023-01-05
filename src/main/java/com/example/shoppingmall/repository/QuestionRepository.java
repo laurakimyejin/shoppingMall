58a97f522aebd8dc78a933393c70b67130312dc2
angpang2 <whtjdcks1111@naver.com>
2023-01-05 11:08:59 +0900

    고객센터 질문 검색및 페이징

--- src/main/java/com/example/shoppingmall/repository/QuestionRepository.java
+++ src/main/java/com/example/shoppingmall/repository/QuestionRepository.java
@@ -1,7 +1,17 @@
 package com.example.shoppingmall.repository;
 
 import com.example.shoppingmall.entity.QuestionEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<QuestionEntity,Long> {
+
+    Page<QuestionEntity> findByQuestionTitleContaining(String search, PageRequest of);
+
+    Page<QuestionEntity> findByQuestionContentsContaining(String search, PageRequest of);
+
+    Page<QuestionEntity> findByQuestionNameContaining(String search, PageRequest of);
 }
