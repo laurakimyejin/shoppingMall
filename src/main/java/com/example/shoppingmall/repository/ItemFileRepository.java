f4534a3957e66b563a12d70e2346f08925e206de
alswjd094 <alswjd094@gmail.com>
2023-01-05 10:31:14 +0900

    상품수정 파일첨부 수정

--- src/main/java/com/example/shoppingmall/repository/ItemFileRepository.java
+++ src/main/java/com/example/shoppingmall/repository/ItemFileRepository.java
@@ -1,7 +1,14 @@
 package com.example.shoppingmall.repository;
 
+import com.example.shoppingmall.entity.ItemEntity;
 import com.example.shoppingmall.entity.ItemFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemFileRepository extends JpaRepository<ItemFileEntity,Long> {
+
+    List<ItemEntity> findByItemEntity(ItemEntity itemEntity2);
+
+    void deleteByItemEntity(ItemEntity itemEntity2);
 }
