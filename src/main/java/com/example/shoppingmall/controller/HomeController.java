f1ccf97617f763fcb0a4b7c960358ad35e62ade8
angpang2 <whtjdcks1111@naver.com>
2023-01-06 13:08:08 +0900

    메인화면 수정2

--- src/main/java/com/example/shoppingmall/controller/HomeController.java
+++ src/main/java/com/example/shoppingmall/controller/HomeController.java
@@ -1,19 +1,40 @@
 package com.example.shoppingmall.controller;
 
+import com.example.shoppingmall.dto.ItemDTO;
 import com.example.shoppingmall.dto.MemberDTO;
+import com.example.shoppingmall.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    final ItemService itemService;
 
     @GetMapping("/")
-    public String index(){
+    public String findAll(@PageableDefault(page = 1) Pageable pageable, Model model , @RequestParam(required = false , value = "sort", defaultValue = "id") String sort){
+        Page<ItemDTO> itemDTOList = itemService.findAll(pageable, sort);
+        model.addAttribute("itemList",itemDTOList);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int endPage = ((startPage + blockLimit - 1) < itemDTOList.getTotalPages()) ? startPage + blockLimit - 1 : itemDTOList.getTotalPages();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("sort", sort);
+        model.addAttribute("size", pageable.getPageSize());
+        model.addAttribute("page", pageable.getPageNumber());
+        System.out.println("itemList" + itemDTOList);
+
+
         return "index";
     }
 
