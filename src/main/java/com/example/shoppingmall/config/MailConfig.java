3c296690366be6ef5cf8b1fca59ddb9c72638409
angpang2 <whtjdcks1111@naver.com>
2023-01-27 16:50:46 +0900

    이메일 수정

--- src/main/java/com/example/shoppingmall/config/MailConfig.java
+++ src/main/java/com/example/shoppingmall/config/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
 
         javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
         javaMailSender.setUsername("gardening2023"); // 네이버 아이디
-        javaMailSender.setPassword("rmqtn04--"); // 네이버 비밀번호
+        javaMailSender.setPassword("rmqtn06--"); // 네이버 비밀번호
 
         javaMailSender.setPort(465); // 메일 인증서버 포트
 
